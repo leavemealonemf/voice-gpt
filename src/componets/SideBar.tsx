aa26a7086041cd38e21db3d1e7777833b0f8253a
leavemealonemf <strangemisterio78@gmail.com>
2023-11-02 16:05:25 +0300

    added home navigate

--- src/componets/SideBar.tsx
+++ src/componets/SideBar.tsx
@@ -18,7 +18,10 @@ const SideBar: FC = () => {
     <div className={`text-white min-h-screen w-72`}>
 
       <div className='flex flex-col items-center justify-center mt-7'>
-        <span className='text-center mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white'>VoiceGPT</span>
+        <span
+          onClick={() => navigate('/')} 
+          className='cursor-pointer text-center mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white
+        '>VoiceGPT</span>
         <span className='text-sm font-normal text-gray-500 dark:text-gray-400 -mt-3'>Version beta 0.1</span>
       </div>
 
